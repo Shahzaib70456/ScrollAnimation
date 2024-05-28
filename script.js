7a3a17236c6792db7639b5ee10b4bdb177e07c2e
Shahzaib70456 <mh7024@nyu.edu>
2024-05-28 22:11:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('scroll', function() {
+    const parallaxSections = document.querySelectorAll('.parallax-section');
+    parallaxSections.forEach(section => {
+        let offset = window.pageYOffset;
+        section.style.backgroundPositionY = offset * 0.7 + 'px';
+    });
+});
